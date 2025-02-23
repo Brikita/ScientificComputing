0a1f52c2fdf34570df86f5f44f61f1b52d734ff3
Brian <briankinyua0101@gmail.com>
2025-02-23 10:45:39 +0300

    control structures

---
+++ python_basics/control_structures.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number: "))
+if (num%2==0):
+    print ("Even")
+else:
+    print ("Odd")
+
+list1 = []
+for i in range(1, 50):
+    if i%2==0:
+        list1.append(i)
+print (list1)
+
+a=10
+while (a>0):
+    print (a)
+    a-=1
+
