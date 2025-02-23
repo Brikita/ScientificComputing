c61d0a9c2883b9843b95c4c8f0602a229dbe6150
Brian <briankinyua0101@gmail.com>
2025-02-23 10:45:21 +0300

    python data types

---
+++ python_basics/datatypes.py
@@ -0,0 +1,39 @@
+#int
+a=2
+#float
+b=2.5
+#complex
+c=2+3j
+#list
+d=[1,2,3,4,5]
+#tuple
+e=(1,2,3,4,5)
+#dict
+f={1:'a',2:'b',3:'c'}
+#set
+g={1,2,3,4,5}
+#bool
+h=True
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+
+#int to float
+a=float(a)
+print(type(a))
+#float to int
+b=int(b)
+print(type(b))
+#int to complex
+a=complex(a)
+print(type(a))
+#complex to int
+c=int(c)
+print(type(c))
